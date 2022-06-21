aecc2a57fba51e55cb07aaeb11c079fd5db76a65
lunia56 <101800801+lunia56@users.noreply.github.com>
2022-06-21 19:49:09 +0300

    Updates

---
+++ precache-manifest.ec17fe8b32e9fb96b8ffe289b5e1f3d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaaf45fb0708c48b3db2",
+    "url": "/react-gh-pages/static/js/main.eaaf45fb.chunk.js"
+  },
+  {
+    "revision": "c21c56453abcf03d92fe",
+    "url": "/react-gh-pages/static/css/1.e57023e7.chunk.css"
+  },
+  {
+    "revision": "c21c56453abcf03d92fe",
+    "url": "/react-gh-pages/static/js/1.c21c5645.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "75b606dc2649547b52ec45c503ecb289",
+    "url": "/react-gh-pages/index.html"
+  }
+];
